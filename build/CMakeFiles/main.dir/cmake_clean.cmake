aa2c59f612ab460c220011f0050ab98ad95c0315
Mexanobar <mexanobar@gmail.com>
2021-12-13 00:11:09 +0300

    deleted old executable

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/src/main.cpp.o"
+  "CMakeFiles/main.dir/src/shake_compensation.cpp.o"
+  "CMakeFiles/main.dir/src/Canny_Hough.cpp.o"
+  "CMakeFiles/main.dir/src/Gauss_threshold.cpp.o"
+  "main.pdb"
+  "main"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
